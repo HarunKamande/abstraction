ccb6eafa6f0ed43df0a52909c8a216d2d347d31f
harun <kamandeharun99@mail.com>
2024-04-13 01:31:15 +0300

    Main update

---
+++ Main.java
@@ -0,0 +1,15 @@
+public class Main {
+
+    public static void main(String[] args) {
+
+        //square Instance
+        Square s1 = new Square(23, 56);
+        s1.display();
+        s1.calculate();
+
+        //Rhombus instance creation
+        Rhombus r1=new Rhombus(3,9);
+        r1.display();
+        r1.calculate();
+    }
+}
